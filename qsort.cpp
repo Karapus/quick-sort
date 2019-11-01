48969bc8350ccd237d9e75b0dba9b50efcf24d9f
Karapus <evgeny.nay@outlook.com>
2019-11-01 12:02:34 +0300

    support of any type

--- qsort.cpp
+++ qsort.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <assert.h>
 
-void swap(void* a, void* b, size_t size)
+void swap(void *a, void *b, size_t size)
 {
 	assert(a);
 	assert(b);
@@ -19,28 +19,30 @@ int intCmp(const void *a, const void *b)
 	return *((const int *)a) - *((const int *)b);
 }
 
-void qsort(int* arr, size_t len, size_t size, int cmp(const void *, const void *))
+void qsort(void *arr, size_t len, size_t size, int (*cmp)(const void *, const void *))
 {
 	assert(arr);
 	assert(size);
 
 	if (len <= 1) return;
 
-	swap(arr + rand() % len, arr, size);
-	int* cur_l = arr + 1;
-	int* cur_r = arr + (len - 1);
+	swap((char *)arr + size * (rand() % len), arr, size);
+	char *cur_l = (char *)arr + size;
+	char *cur_r = (char *)arr + size * (len - 1);
+	
 	while (cur_l <= cur_r)
 	{
-		if 	(cmp(cur_l, arr) < 0) cur_l ++;
-		else if (cmp(cur_r, arr) > 0) cur_r --;
-		else swap(cur_l ++, cur_r --, size);
+		if 	(cmp(cur_l, arr) < 0) cur_l += size;
+		else if (cmp(cur_r, arr) > 0) cur_r -= size;
+		else 
+		{
+			swap(cur_l, cur_r, size);
+			cur_l += size, cur_r -= size;
+		}
 	}
-
-	for (size_t i = 0; i < len; printf("%d, ", ((int*)arr)[i++]))
-		;
-	putchar('\n');
-	return 	qsort(arr, cur_r - arr + 1, size, cmp),
-		qsort(cur_l, len - (cur_l - arr), size, cmp);
+	
+	return 	qsort(arr, (cur_r - (char *)arr) / size + 1, size, cmp),
+		qsort(cur_l, len - (cur_l - (char *)arr) / size, size, cmp);
 }
 
 int main()
